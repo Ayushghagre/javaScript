fed08e0e605d2d8ee947d98e4342bc72cea5a882
Ayush Mahesh Ghagre <aghagre@catchpoint.com>
2023-12-18 15:56:46 +0530

    file added

---
+++ removed_element.js
@@ -0,0 +1,13 @@
+// JavaScript code to illustrate pop() function 
+// to remove array elements 
+
+function func() { 
+	let arr = ["shift", "splice", "filter", "pop"]; 
+
+	// Popping the last element from the array 
+	let popped = arr.pop(); 
+	console.log("Removed element: " + popped); 
+	console.log("Remaining elements: " + arr); 
+} 
+func();
+cd
